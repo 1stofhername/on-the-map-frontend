4830444532bc1f7c57c056a9e8c67de2832de96c
Alicia <arrowsrp@yahoo.com>
2022-08-11 12:12:13 -0700

    Adds classnames to component divs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 export default function NavBar () {
     return (
-        <div className="component-bar" id="navbar">
+        <div className="bar" id="navbar">
             <h3>NavBar</h3>
             <Link to="/">Home</Link>
             <Link to="discover">Discover</Link>
--- src/components/ProfileBar.js
+++ src/components/ProfileBar.js
@@ -1,5 +1,7 @@
 export default function ProfileBar () {
     return (
-        <h3>Some profile info</h3>
+        <div className="bar" id="profile">
+            <h3>Some profile info</h3>
+        </div>
     )
 }
