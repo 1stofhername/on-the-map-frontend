59f026272d2e046a8b83e1c29b72a1e0b484df48
Alicia <arrowsrp@yahoo.com>
2022-08-11 12:09:02 -0700

    Adds PostsContainer component

---
+++ src/components/PostsContainer.js
@@ -0,0 +1,7 @@
+export default function PostsContainer () {
+    return (
+        <div className="container" id="posts">
+            <h3>Here are some rendered posts of the selected Mapper or a default image</h3>
+        </div>
+    )
+}
