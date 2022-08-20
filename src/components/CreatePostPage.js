8ca826ef2f5b2640e018acc45025436d7823f554
Alicia <arrowsrp@yahoo.com>
2022-08-20 14:18:04 -0700

    Creates form in CreatePostPage and formData state

--- src/components/CreatePostPage.js
+++ src/components/CreatePostPage.js
@@ -1,5 +1,31 @@
+import { useState } from 'react';
+
 export default function CreatePostPage () {
+
+    const [formData, setFormData] = useState({
+        caption:null,
+        img_link:null
+    })
+
     return (
-        <h3>Create a post!</h3>
+        <div className="create-post">
+        <h3>Create an update!</h3>
+        <form className="new-post-form">
+            <label>
+                Add a photo:
+                <input onChange={(e)=>{console.log(e.target.value)}} type="file" name="image" />
+            </label>
+            <label>
+                Caption:
+                <input onChange={(e)=>{console.log(e.target.value)}} type="text" name="title" />
+            </label>
+            <label>
+                Hashtags:
+                <input placeholder='separate tags by #, no spaces.' type="text" />
+            </label>
+            <input type="submit" value="Post" />
+        </form>
+        </div>
+        
     )
 }
