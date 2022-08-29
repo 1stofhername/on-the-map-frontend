15b2562d17dd83e05fbe1ad36116b931692e610e
Alicia <arrowsrp@yahoo.com>
2022-08-29 12:57:25 -0700

    Adds login handling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import './App.css';
 import NavBar from "./components/NavBar";
 import Homepage from "./components/Homepage";
 import DiscoverPage from './components/DiscoverPage';
-import CreatePostPage from './components/CreatePostPage';
+import CreatePostPage from './components/CreatePost';
 import LoginPage from './components/LoginPage';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (loginFormData) => {
+    const [email, password] = loginFormData;
+    console.log("email:" `${email}`, "password:" `${password}`);
+    setIsLoggedIn(true);
+    // fetch("http://localhost:9292/")
+    
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h4>On The Map</h4>
       </header>
-      <NavBar />
+      {isLoggedIn ? <NavBar /> : null}
+      {!isLoggedIn? <LoginPage /> : null }
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/home" element={<Homepage logInStatus={isLoggedIn} />} />
         <Route path="/discover" element={<DiscoverPage />} />
         <Route path="/create-post" element={ <CreatePostPage />} />
-        <Route path='/login' element={ <LoginPage />} /> 
+        <Route path='/login' element={ <LoginPage handleLogin={handleLogin} />} /> 
       </Routes>
     </div>
   );
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,22 +1,46 @@
 import { useState } from 'react';
 
-export default function LoginPage () {
+export default function LoginPage ( handleLogin ) {
+
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const loginFormData = {
+        email: `${email}`,
+        password: `${password}`
+    }
+
+    function handleEmailChange (e) {
+        setEmail(e.target.value);
+    }
+
+    function handlePasswordChange (e) {
+        setPassword(e.target.value);
+    }
+
+    function handleSubmit (e) {
+        e.preventDefault();
+        console.log(loginFormData);
+
+        
+    }
 
     return (
         <div className="page" id="login-page">
 
             <h2>Login</h2>
-            <form className="login-form">
+            <form className="login-form" onSubmit={handleSubmit}>
                 <label>
                     Email:
-                    <input type="text" name="email" placeholder="johnappleseed@email.com" onChange={(e)=>{console.log(e.target.value)}} />
+                    <input type="text" name="email" placeholder="name@email.com" onChange={handleEmailChange} />
                 </label>
                 <label>
                     Password:
-                    <input type="text" name="password" placeholder="********" onChange={(e)=>{console.log(e.target.value)}} />
+                    <input type="text" name="password" placeholder="Password" onChange={handlePasswordChange} />
                 </label>
+                <input type="submit" value="Log In" />
             </form>
-            <button>Sign Up.</button>
+            
+            <p>Sign Up.</p>
         </div>
     )
 }
